6a15cabd7091163da47d4d1fb1f55232e7dccf3e
bdamja <iceicebabyalter@gmail.com>
2022-09-22 21:27:00 -0600

    up to date with 1.16.5 branch

--- src/main/java/net/set/spawn/mod/mixin/MinecraftServerMixin.java
+++ src/main/java/net/set/spawn/mod/mixin/MinecraftServerMixin.java
@@ -1,117 +1,21 @@
 package net.set.spawn.mod.mixin;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.ServerTask;
 import net.minecraft.server.WorldGenerationProgressListener;
-import net.minecraft.server.command.CommandOutput;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.snooper.SnooperListener;
-import net.minecraft.util.thread.ReentrantThreadExecutor;
-import net.minecraft.world.dimension.DimensionType;
-import net.set.spawn.mod.Conditionals;
-import net.set.spawn.mod.config.SetSpawnProperties;
-import org.apache.logging.log4j.LogManager;
-import org.jetbrains.annotations.Nullable;
+import net.set.spawn.mod.SetSpawn;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public abstract class MinecraftServerMixin extends ReentrantThreadExecutor<ServerTask> implements SnooperListener, CommandOutput, AutoCloseable, Runnable {
-
-    @Shadow
-    public abstract ServerWorld getWorld(DimensionType dimensionType);
-
-    private static final double[] coordinates = new double[3];
-
-    public MinecraftServerMixin(String string) {
-        super(string);
-    }
+public abstract class MinecraftServerMixin{
 
     @Inject(method = "prepareStartRegion", at = @At(value = "HEAD"))
-    public void validateSpawn(WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci){
-        Conditionals.isModActive = false;
-        if (properSpawnEditingCircumstances(this.getWorld(DimensionType.OVERWORLD))) {
-            Conditionals.isModActive = true;
-            double x = coordinates[0];
-            double y = coordinates[1];
-            double z = coordinates[2];
-            LogManager.getLogger("SetSpawn").info("Overriding player spawnpoint to " + x + " " + y + " " + z);
+    public void setspawnmod_startedWorldGen(WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci){
+        if (SetSpawn.config.isEnabled()) {
+            SetSpawn.shouldModifySpawn = true;
         }
-    }
-
-    private boolean properSpawnEditingCircumstances(ServerWorld world){
-        if (!isEnabled()) return false;
-        if (!isWorldLoading()) return false;
-        if (!isSetSeed(world)) return false;
-        if (!loadCoordinates()) return false;
-        if (!areCoordinatesPossible(world)) return false;
-        return true;
-    }
-
-    private boolean isEnabled() {
-        if (!SetSpawnProperties.enabled) {
-            LogManager.getLogger("SetSpawn").info("Set Spawn is currently disabled.");
-        }
-        return SetSpawnProperties.enabled;
-    }
-
-    private boolean isWorldLoading(){
-        return Conditionals.isAWorldGenerating;
-    }
-
-    private boolean isSetSeed(ServerWorld world) {
-        return String.valueOf(world.getSeed()).equals(SetSpawnProperties.seed);
-    }
-
-    private boolean loadCoordinates() {
-        String[] coordinatesStringArray = SetSpawnProperties.coordinates.split(" ");
-        if (coordinatesStringArray.length != 3) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were in an invalid format. Not overriding player spawnpoint.");
-            return false;
-        }
-        try {
-            coordinates[0] = Double.parseDouble(coordinatesStringArray[0]);
-            coordinates[1] = Double.parseDouble(coordinatesStringArray[1]);
-            coordinates[2] = Double.parseDouble(coordinatesStringArray[2]);
-        } catch (NumberFormatException e) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were in an invalid format. Not overriding player spawnpoint.");
-            return false;
-        }
-        return true;
-    }
-
-    private boolean areCoordinatesPossible(ServerWorld world) {
-        double x = coordinates[0];
-        double y = coordinates[1];
-        double z = coordinates[2];
-        if ( (Math.abs(x) % 1 != 0.5) || (Math.abs(y) % 1 != 0.0) || (Math.abs(z) % 1 != 0.5) ) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were impossible. Make sure the coordinates are in <X>.5 <Y>.0 <Z>.5 format. Not overriding player spawnpoint.");
-            return false;
-        }
-
-        int spawnX = world.getSpawnPos().getX();
-        int spawnZ = world.getSpawnPos().getZ();
-        if ( (Math.abs(x - spawnX) > 10) || (Math.abs(z - spawnZ) > 10) ) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were impossible. Make sure X and Z are not more than 10 blocks away from the world spawn. Not overriding player spawnpoint.");
-            return false;
-        }
-
-        BlockPos setCoords = new BlockPos(x, y, z);
-        double overworldSpawnY = findOverworldSpawn(world, setCoords.getX(), setCoords.getZ()).getY();
-        if (overworldSpawnY != y) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were impossible. Make sure the Y coordinate is possible. Not overriding player spawnpoint.");
-            return false;
-        }
-        return true;
-    }
-
-    @Nullable
-    private static BlockPos findOverworldSpawn(ServerWorld world, int x, int z) { // method taken from mc code
-        return world.getDimension().getTopSpawningBlockPosition(x, z, false);
     }
 }
 
--- src/main/java/net/set/spawn/mod/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/set/spawn/mod/mixin/ServerPlayerEntityMixin.java
@@ -3,13 +3,17 @@ package net.set.spawn.mod.mixin;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.container.ContainerListener;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import net.set.spawn.mod.Conditionals;
-import net.set.spawn.mod.config.SetSpawnProperties;
+import net.set.spawn.mod.Seed;
+import net.set.spawn.mod.SetSpawn;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,20 +21,42 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity implements ContainerListener {
 
+    @Shadow @Final public MinecraftServer server;
+
     public ServerPlayerEntityMixin(World world, GameProfile profile) {
         super(world, profile);
     }
 
     @Inject(method = "moveToSpawn", at = @At("HEAD"), cancellable = true)
-    public void setSpawn(ServerWorld world, CallbackInfo ci) {
-        if (Conditionals.isModActive && Conditionals.isAWorldGenerating) {
-            String[] coordinates = SetSpawnProperties.coordinates.split(" ");
-            double x = Double.parseDouble(coordinates[0]);
-            double y = Double.parseDouble(coordinates[1]);
-            double z = Double.parseDouble(coordinates[2]);
-            this.refreshPositionAndAngles(new BlockPos(x, y, z), 0.0F, 0.0F);
-            Conditionals.isAWorldGenerating = false;
-            ci.cancel();
+    public void setspawnmod_setSpawn(ServerWorld world, CallbackInfo ci) {
+        if (SetSpawn.shouldModifySpawn) {
+            SetSpawn.shouldModifySpawn = false;
+            Seed seedObject = SetSpawn.findSeedObjectFromLong(world.getSeed());
+            String response;
+            if (seedObject != null ) {
+                int xFloor = MathHelper.floor(seedObject.getX());
+                int zFloor = MathHelper.floor(seedObject.getZ());
+                if ((Math.abs(xFloor - world.getSpawnPos().getX()) > this.server.getSpawnRadius(world))
+                        || (Math.abs(zFloor - world.getSpawnPos().getZ()) > this.server.getSpawnRadius(world))) {
+                    SetSpawn.shouldSendErrorMessage = true;
+                    response = "The X or Z coordinates given (" + seedObject.getX() + ", " + seedObject.getZ() + ") are more than 10 blocks away from the world spawn. Not overriding player spawnpoint.";
+                    SetSpawn.errorMessage = response;
+                    SetSpawn.LOGGER.warn(response);
+                } else {
+                    BlockPos spawnPos = world.getDimension().getTopSpawningBlockPosition(xFloor, zFloor, false);
+                    if (spawnPos != null) {
+                        SetSpawn.shouldSendErrorMessage = false;
+                        SetSpawn.LOGGER.info("Spawning player at: " + seedObject.getX() + " " + spawnPos.getY() + " " + seedObject.getZ());
+                        this.refreshPositionAndAngles(spawnPos, 0.0F, 0.0F);
+                        ci.cancel();
+                    } else {
+                        SetSpawn.shouldSendErrorMessage = true;
+                        response = "There is no valid spawning location at the specified coordinates (" + seedObject.getX() + ", " + seedObject.getZ() + "). Not overriding player spawnpoint.";
+                        SetSpawn.errorMessage = response;
+                        SetSpawn.LOGGER.warn(response);
+                    }
+                }
+            }
         }
     }
 
