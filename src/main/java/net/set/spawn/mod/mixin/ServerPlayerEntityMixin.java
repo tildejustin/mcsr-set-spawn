6858364b18b7923874702fc676dd7781445be6e4
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-02-08 21:51:46 -0500

    fix impossible spawns on non-config seeds, fix spawn validations being off by one

--- src/main/java/net/set/spawn/mod/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/set/spawn/mod/mixin/ServerPlayerEntityMixin.java
@@ -4,6 +4,7 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.set.spawn.mod.Seed;
@@ -17,6 +18,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     private int xFloor;
     private int zFloor;
+    Seed seedObject;
 
     public ServerPlayerEntityMixin(World world, GameProfile profile) {
         super(world, profile);
@@ -26,12 +28,14 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     private int setspawn_setX(int x) {
         if (SetSpawn.shouldModifySpawn) {
             SetSpawn.shouldModifySpawn = false;
-            Seed seedObject = SetSpawn.findSeedObjectFromLong(this.world.getSeed());
+            seedObject = SetSpawn.findSeedObjectFromLong(this.world.getSeed());
             String response;
             if (seedObject != null) {
                 xFloor = MathHelper.floor(seedObject.getX());
                 zFloor = MathHelper.floor(seedObject.getZ());
-                if ((Math.abs(xFloor - this.world.getWorldSpawnPos().x) > MinecraftServer.getServer().getSpawnProtectionRadius() - 6) || (Math.abs(zFloor - this.world.getWorldSpawnPos().z) > MinecraftServer.getServer().getSpawnProtectionRadius() - 6)) {
+                BlockPos spawnPos = this.world.getWorldSpawnPos();
+                int radius = MinecraftServer.getServer().getSpawnProtectionRadius() - 6;
+                if (Math.abs(xFloor + 0.5 - spawnPos.x) > radius || Math.abs(zFloor + 0.5 - spawnPos.z) > radius) {
                     SetSpawn.shouldSendErrorMessage = true;
                     response = "The X or Z coordinates given (" + seedObject.getX() + ", " + seedObject.getZ() + ") are more than 10 blocks away from the world spawn. Not overriding player spawnpoint.";
                     SetSpawn.errorMessage = response;
@@ -45,7 +49,7 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
     @ModifyVariable(method = "<init>", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/world/ServerWorld;method_3708(II)I"), ordinal = 1)
     private int setspawn_setZ(int z) {
-        if (SetSpawn.shouldSendErrorMessage) {
+        if (SetSpawn.shouldSendErrorMessage || seedObject == null) {
             return z;
         }
         return zFloor;
@@ -53,9 +57,9 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
     @ModifyVariable(method = "<init>", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/world/ServerWorld;method_3708(II)I"), ordinal = 2)
     private int setspawn_setY(int y) {
-        if (SetSpawn.shouldSendErrorMessage) {
+        if (SetSpawn.shouldSendErrorMessage || seedObject == null) {
             return y;
         }
-        return MinecraftServer.getServer().getWorld().method_3708(xFloor, zFloor);
+        return world.method_3708(xFloor, zFloor);
     }
 }
