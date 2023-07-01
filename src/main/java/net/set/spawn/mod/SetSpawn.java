1a6be87507ccd53eba2d1e96b1aa01db7c1ae491
tildejustin <spacepepper3.14@gmail.com>
2023-07-01 19:30:02 -0400

    1.18 only now

--- src/main/java/net/set/spawn/mod/SetSpawn.java
+++ src/main/java/net/set/spawn/mod/SetSpawn.java
@@ -13,19 +13,19 @@ import java.io.*;
 import java.util.Objects;
 
 public class SetSpawn implements ClientModInitializer {
-    public static Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "setspawnmod";
+    public static final String subDir = SetSpawn.MOD_ID + "_global";
+    public static Logger LOGGER = LogManager.getLogger();
     public static boolean shouldModifySpawn;
     public static boolean shouldSendErrorMessage;
     public static String errorMessage;
-    public static final String subDir = SetSpawn.MOD_ID + "_global";
     public static File localConfigFile;
     public static File globalConfigFile;
     public static Config config;
 
     private static void createIfNonExistent(File file) {
         try {
-            if(file.createNewFile()){
+            if (file.createNewFile()) {
                 writeDefaultProperties(file);
             }
         } catch (IOException e) {
@@ -44,15 +44,7 @@ public class SetSpawn implements ClientModInitializer {
     }
 
     private static void writeDefaultProperties(File file) throws IOException {
-        Seed vine = new Seed("8398967436125155523", "vine", -201.5, 229.5);
-        Seed taiga = new Seed("2483313382402348964", "taiga", -233.5, 249.5);
-        Seed gravel = new Seed("-3294725893620991126", "gravel", 161.5, 194.5);
-        Seed dolphin = new Seed("-4530634556500121041", "dolphin", 174.5, 201.5);
-        Seed treasure = new Seed("7665560473511906728", "treasure", 90.5, 218.5);
-        Seed rng = new Seed("-4810268054211229692", "rng", -153.5, 234.5);
-        Seed arch = new Seed("2613428371297940758", "arch", -154.5, -217.5);
-        Seed fletcher = new Seed("2478133068685386821", "fletcher", -248.5, 106.5);
-        Seed[] seedsToWrite = new Seed[] { vine, taiga, gravel, dolphin, treasure, rng, arch, fletcher };
+        Seed[] seedsToWrite = new Seed[]{};
         Config config = new Config(true, false, seedsToWrite);
 
         try (Writer writer = new FileWriter(file)) {
@@ -64,6 +56,17 @@ public class SetSpawn implements ClientModInitializer {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static Seed findSeedObjectFromLong(long seedLong) {
+        String seed = String.valueOf(seedLong);
+        Seed[] seedObjects = config.getSeeds();
+        for (Seed seedObject : seedObjects) {
+            if (Objects.equals(seedObject.getSeed(), seed)) {
+                return seedObject;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -80,17 +83,6 @@ public class SetSpawn implements ClientModInitializer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static Seed findSeedObjectFromLong(long seedLong) {
-        String seed = String.valueOf(seedLong);
-        Seed[] seedObjects = config.getSeeds();
-        for (Seed seedObject : seedObjects) {
-            if (Objects.equals(seedObject.getSeed(), seed)) {
-                return seedObject;
-            }
-        }
-        return null;
     }
 
 }
